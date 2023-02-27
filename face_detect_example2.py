3727c60cb989ba6b47ee1cc216d423981e537bb8
Brian Huang <huang.brian.c@gmail.com>
2023-02-26 18:10:15 -0600

    Create face_detect_example2.py

---
+++ face_detect_example2.py
@@ -0,0 +1,38 @@
+# This example will open up the webcam and refresh the window continuously until the 'q' key is pressed to close.
+import cv2
+
+# Read in the cascade file for face detect
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+# Open up webcam and repeat and draw rectangles around faces continuously
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        
+        flags=cv2.CASCADE_SCALE_IMAGE   #cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Webcam -- Press \'q\' key to quit', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
